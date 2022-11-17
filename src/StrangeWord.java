d72deaaf7d3dd57d41547ef20b0020a8f175fc29
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-17 12:31:06 -0500

    period end

--- src/StrangeWord.java
+++ src/StrangeWord.java
@@ -1,2 +1,88 @@
-package PACKAGE_NAME;public class StrangeWord {
+public class StrangeWord {
+
+    private String word;
+
+    /* Constructor
+     *
+     * PRECONDITIONS: word will contain all capital letters
+     *                with no spaces or other non-letter symbols;
+     *                word.length() >= 2
+     */
+    public StrangeWord(String word) {
+        this.word = word;
+    }
+
+    /* getter method for word */
+    public String getWord() {
+        return word;
+    }
+
+    /* This method updates word to include endChar repeated num times
+     * on each end; this method does not return a value, but rather
+     * updates the word instance variable
+     *
+     * PRECONDITIONS:  num > 0 and endChar.length() == 1
+     *
+     */
+    public void bookend(String endChar, int num) {
+        String newWord = "";
+        for (int i = 1; i <= num; i++) {
+            newWord += endChar;
+        }
+        newWord += word;
+        for (int j = 1; j <= num; j++) {
+            newWord += endChar;
+        }
+        word = newWord;
+    }
+
+
+    /* Returns the number of times that word contains searchChar followed
+     * immediately by a character that is not searchChar; this method
+     * does NOT modify word
+     *
+     * PRECONDITION: searchChar.length() == 1
+     */
+    public int followedBy(String searchChar) {
+        int counter = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if ((word.substring(i, i + 1)).equals(searchChar)) {
+                if (i + 2 <= word.length() && !((word.substring(i + 1, i + 2)).equals(searchChar))) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+
+    /* Returns a new string consisting of the letters of word that are
+     * situated at indices that are multiples of offSet; this method
+     * does NOT modify word
+     *
+     * PRECONDITION: offset > 0
+     */
+    public String everyOther(int offSet) {
+        String newString = "";
+        if (word.length() - 1 < offSet) {
+            newString = word.substring(0, 1);
+            return newString;
+        }
+        else {
+            for (int i = 0; i <= word.length() - 1; i += offSet) {
+                newString += word.substring(i, i + 1);
+            }
+            return newString;
+        }
+    }
+
+
+    /* This method prints a word pyramid containing the letters of word;
+     * the pyramid should be built top down, starting with the first
+     * letter and building subsequent levels that are one more character
+     * in length than the previous level; the last level can be does not
+     * need to be filled
+     */
+    //public void wordPyramid()
+    //{ /* to be implemented in part (d) */ }
 }
--- src/StrangeWordRunner.java
+++ src/StrangeWordRunner.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class StrangeWordRunner {
+public class StrangeWordRunner {
+    public static void main(String[] args) {
+        // Testing Part A
+        System.out.println("--- Testing Part A: bookend ---");
+        StrangeWord weird1 = new StrangeWord("BANANAS");
+        weird1.bookend("C", 3);
+        System.out.println(weird1.getWord());
+        weird1.bookend("D", 4);
+        System.out.println(weird1.getWord());
+
+        StrangeWord weird2 = new StrangeWord("CSA");
+        weird2.bookend("Z", 1);
+        System.out.println(weird2.getWord());
+        weird2.bookend("D", 0);
+        System.out.println(weird2.getWord());
+
+        // Testing Part B
+        System.out.println("\n--- Testing Part B: followedBy");
+        StrangeWord weird3 = new StrangeWord("BANANAS");
+        System.out.println(weird3.followedBy("A"));
+        StrangeWord weird4 = new StrangeWord("BAAANNAANAAS");
+        System.out.println(weird4.followedBy("A"));
+        System.out.println(weird4.followedBy("B"));
+        System.out.println(weird4.followedBy("N"));
+        System.out.println(weird4.followedBy("S"));
+        System.out.println(weird4.followedBy("Z"));
+        System.out.println("-- word should NOT be changed: --");
+        System.out.println(weird4.getWord()); // word unchanged
+
+        // Testing Part C
+        System.out.println("\n--- Testing Part C: everyOther ---");
+        StrangeWord weird5 = new StrangeWord("ABCEFGHIJKLMNOP");
+        System.out.println(weird5.everyOther(1));
+        System.out.println(weird5.everyOther(2));
+        System.out.println(weird5.everyOther(3));
+        System.out.println(weird5.everyOther(4));
+        System.out.println(weird5.everyOther(5));
+        System.out.println(weird5.everyOther(6));
+        System.out.println(weird5.everyOther(7));
+        System.out.println(weird5.everyOther(8));
+        System.out.println(weird5.everyOther(9));
+        System.out.println(weird5.everyOther(14));
+        System.out.println(weird5.everyOther(15));
+        System.out.println(weird5.everyOther(16));
+        System.out.println("-- word should NOT be changed: --");
+        System.out.println(weird5.getWord()); // word unchanged
+
+    }
 }
